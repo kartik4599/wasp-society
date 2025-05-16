6293704d5c81d931bada3097acbd7d7dc1123a85
kartik10-24 <kartik.mendu@mindinventory.com>
2025-05-16 16:53:37 +0530

    changes in getSociety query function for "staff" role

--- src/society/backend/querys.ts
+++ src/society/backend/querys.ts
@@ -1,13 +1,23 @@
 import { type GetMySociety } from "wasp/server/operations";
 import { Society } from "wasp/entities";
+import { Role } from "@prisma/client";
 
 export const getSociety: GetMySociety<void, Society | null> = async (
   _,
   ctx
 ) => {
+  const role = ctx.user?.role;
   const { Society } = ctx.entities;
 
-  return Society.findUnique({
-    where: { createdById: ctx.user?.id },
-  });
+  if (Role.owner === role)
+    return Society.findUnique({
+      where: { createdById: ctx.user?.id },
+    });
+
+  if (Role.staff === role && ctx.user?.workingSocietyId)
+    return Society.findUnique({
+      where: { id: ctx.user?.workingSocietyId },
+    });
+
+  return null;
 };
